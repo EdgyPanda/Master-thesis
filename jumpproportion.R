4ff41613ec4b758a48854479ce72c4f03a89ac60
EdgyPanda <emil7200@gmail.com>
2020-09-17 21:05:14 +0200

    Update jumpproportion.R

--- jumpproportion.R
+++ jumpproportion.R
@@ -2,3 +2,47 @@
 
 
 
+source("Previous functions/projectfunctions.R")
+source("functions.R")
+
+library(xts)
+library(highfrequency)
+library(matlib)
+
+dataTLT <- readRDS("dataTLT.rds")
+dataSPY <- readRDS("dataSPY.rds")
+
+
+#open to close test
+
+dataTLT[[1]][c(1,length(dataTLT[[1]])), ]
+
+
+#you only need open-to-close
+
+secs <-c(1,5,15,20,30,60,300,900,1800)
+
+
+frequenciesTLT <- list()
+frequenciesSPY <- list()
+
+tempTLT <- list()
+tempSPY <- list()
+
+
+
+for(j in 1:length(secs)){
+	for(i in 1:length(dataTLT)){
+
+		tempTLT[[i]] <- aggregatets(dataTLT[[i]], on="seconds", k=secs[j])
+		tempSPY[[i]] <- aggregatets(dataSPY[[i]], on="seconds", k=secs[j])
+
+
+	}
+
+	frequenciesTLT[[j]] <- tempTLT
+	frequenciesSPY[[j]] <- tempSPY
+
+
+}
+
