1ee062a85bfe1c7d8894a1bd796398a40be322ef
Emil F. Andersen <emil7200@gmail.com>
2020-09-30 12:45:53 +0200

    Update riskparity.R

--- riskparity.R
+++ riskparity.R
@@ -4,6 +4,12 @@
 source("functions.R")
 library(riskParityPortfolio)
 library(matlib)
+library(xts)
+library(highfrequency)
+library(matlib)
+library(MCS)
+
+
 
 riskparity_2dim <- function(matrix, risktarget, rt = F){
   
@@ -15,9 +21,11 @@ riskparity_2dim <- function(matrix, risktarget, rt = F){
 
   w <- matrix(c(w_1, w_2), ncol=1, nrow=2) #dimnames = list(c(), c("Bond", "Stock"))
   
-  portrisk <- sqrt(t(w) %*% (matrix) %*% w) 
+  portrisk <- as.numeric(sqrt(t(w) %*% (matrix) %*% w)) 
   
-  riskcont <- c(sqrt(bonds)*w_1, sqrt(stocks)*w_2)
+  riskcont <-  (w * (matrix %*% w)) / portrisk
+
+  relativeriskcont <- (w * (matrix %*% w)) / portrisk^2
 
   if(rt){
 
@@ -25,7 +33,11 @@ riskparity_2dim <- function(matrix, risktarget, rt = F){
 
   	w_new <- w %*% alpha
 
-  	portrisk <-  w_new %*% portrisk  #gives marginal risk for each asset. 
+  	w_new <- matrix(c(w_new[1], w_new[2]), ncol=1, nrow=2)
+
+  	portrisk <-  as.numeric(sqrt(t(w_new) %*% matrix %*% w_new))  #gives marginal risk for each asset. 
+
+  	riskcont <- (w_new * (matrix %*% w_new)) / portrisk
 
   	lout <- list(w_new, portrisk, riskcont)
   	
@@ -34,8 +46,8 @@ riskparity_2dim <- function(matrix, risktarget, rt = F){
  	return(lout)
   }
 
-  lout <- list(w, portrisk, riskcont)
-  names(lout) <- c("w", "portrisk", "riskcont")
+  lout <- list(w, portrisk, riskcont, relativeriskcont)
+  names(lout) <- c("w", "portrisk", "riskcont", "relativeriskcont")
 
   return(lout)
   
@@ -45,24 +57,53 @@ riskparity_2dim <- function(matrix, risktarget, rt = F){
 
 calccov <- readRDS("calculatedcovariances.rds")
 
-weightsrp <- matrix(0L, nrow = 100, ncol = 2)
-
-weightspalomar <-  matrix(0L, nrow = 100, ncol = 2)
+mergedfrequencies <- readRDS("mergedfrequencies.rds")
 
 
-for(i in 1:100){
+#-------------------------------portfolio volatility's sensitivity to correlation--------------------------
 
-weightsrp[i,] <- riskparity_2dim(calccov[[1]][[7]][,,i], 0.1, T)$portrisk
+#getting average vol for tlt and spy.
 
-weightspalomar[i, ] <- riskParityPortfolio(calccov[[1]][[1]][,,i])$w
+meanvolTLT <- mean(sqrt(calccov[[1]][[7]][1,1,]*252*(24/6.5)))
+
+meanvolSPY <-  mean(sqrt(calccov[[1]][[7]][2,2,]*252*(24/6.5)))
+
+correlations <- seq(-0.9,0.9,0.01)
+
+sensitivity <- numeric()
+
+sensitivity2 <- numeric()
+
+for(i in 1:length(correlations)){
+
+	newcovariance <- matrix(c(meanvolTLT^2, meanvolTLT*meanvolSPY*correlations[i],
+		meanvolTLT*meanvolSPY*correlations[i], meanvolSPY^2), ncol = 2, nrow = 2)
+
+	w2 <- riskparity_2dim(newcovariance,0,F)$w
+
+	portrisk2 <- riskparity_2dim(newcovariance,0,F)$portrisk
+
+	sensitivity2[i] <-  (w2[1]*w2[2]*meanvolTLT*meanvolSPY)  /  (portrisk2)
+
 
 }
 
-riskparity_2dim(calccov[[1]][[1]][,,1])$riskcont
+
+library(ggplot2)
 
 
+#Shows the volatility's sensitivity to correlation for the unlevered risk-parity portfolio. In essence, 
+#upscaling and downscaling the portfolios using a leverage parameter only shifts the graph.
 
-lel <- matrix(c(diag(calccov[[1]][[1]][,,1])[1],0,0,diag(calccov[[1]][[1]][,,1])[2]), ncol = 2, nrow = 2)
+ggplot() + geom_line(aes(correlations, sensitivity2), col = "red", lwd = 1) + 
+  scale_x_continuous(breaks = round(seq(-0.9,0.9, by = 0.1),1)) + ylab("portfolio volatility (%)")
 
-minvar(lel)
+
+(riskparity_2dim(newcovariance, 0.1,T)$w) * (newcovariance %*% riskparity_2dim(newcovariance, 0.1,T)$w)/0.1
+
+riskparity_2dim(newcovariance, 0.1,F)
+
+riskparity_2dim(newcovariance, 0.1,T)
+
+
 
