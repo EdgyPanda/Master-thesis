a48df993eec34928ac25233dbd2a6cb382074e33
Emil F. Andersen <emil7200@gmail.com>
2021-01-28 14:17:57 +0100

    Update functions.R

--- functions.R
+++ functions.R
@@ -329,7 +329,7 @@ min.qlike.riskmetrics <- function(cov, half.life, proxy, correlation = F, lambda
 
 ########################################################################################################################
 #
-#								REALIZED AND INTERNAL FUNCTIONS FOR REALIZED MEASURES
+#						REALIZED AND INTERNAL FUNCTIONS FOR REALIZED MEASURES
 #
 ########################################################################################################################
 
@@ -398,7 +398,7 @@ bandwidthH <- function(list, sparsedata){
 
 ########################################################################################################################
 #
-#													GENERAL FUNCTIONS
+#						GENERAL FUNCTIONS
 #
 ########################################################################################################################
 
@@ -670,7 +670,7 @@ portconcentration <- function(weights){
 
 ########################################################################################################################
 #
-#										DRD-HAR MODEL AND CORRELATION MODELS. 
+#						DRD-HAR MODEL AND CORRELATION MODELS. 
 #
 ########################################################################################################################
 
@@ -865,7 +865,7 @@ EstimatecorrHAR2 <- function(returns, variances, proxy, trace=1, ineqfun = ineqc
 
 ########################################################################################################################
 #
-#										REALIZED SCALAR-BASED BIVARIATE GARCH MODEL (rBG)
+#						REALIZED SCALAR-BASED BIVARIATE GARCH MODEL (rBG)
 #
 ########################################################################################################################
 
@@ -1058,7 +1058,7 @@ EstimateBivarGARCH <- function(dailyret, covariance, bootstrap = FALSE, vPar=NUL
 
 ########################################################################################################################
 #
-#								CONTINUOUS ASSYMMETRICAL REALIZED SCALAR-BASED GARCH MODEL (crBG)
+#						CONTINUOUS ASSYMMETRICAL REALIZED SCALAR-BASED GARCH MODEL (crBG)
 #
 ########################################################################################################################
 
@@ -1278,7 +1278,7 @@ EstimateBivarGARCHContAsym <- function(dailyret, covariance, ineqfun_GARCH = ine
 
 ########################################################################################################################
 #
-#													REALIZED DCC MODEL (rDCC)
+#						REALIZED DCC MODEL (rDCC)
 #
 ########################################################################################################################
 
@@ -1492,7 +1492,7 @@ Estimate_rDCC <- function(mY, covariance, getDates, bootstrap = F, residuals = N
 
 ########################################################################################################################
 #
-#										CONTINUOUS ASSYMMETRICAL REALIZED DCC MODEL (crDCC)
+#						CONTINUOUS ASSYMMETRICAL REALIZED DCC MODEL (crDCC)
 #
 ########################################################################################################################
 
